3b8f47eb39f8f73bc1d0a2e8477ca4bbe6532c4d
Barak Doron <barakdoron3@gmail.com>
2026-09-05 23:44:22 +0300

    add restriction for non Linux users

--- src/logic/helpers/validator.py
+++ src/logic/helpers/validator.py
@@ -1,10 +1,11 @@
+from sys import platform
 from preferences import *
 
 def validate_user_preferences():
   if task_number not in [1,2]:
     raise ValueError("<task_number> must be set to 1 or 2")
-  if not isinstance(sampling_interval,(int, float)) or sampling_interval <=0:
-    raise ValueError("<ampling_interval> must be a positive number")
+  if not isinstance(sampling_interval,(int, float)) or sampling_interval < 0.2:
+    raise ValueError("<ampling_interval> must be a positive number and at least 0.2 seconds")
   if not isinstance(sliding_window_size,(int)):
       raise ValueError("<sliding_window_size> must be an integer number")
   if not isinstance(parameters_dict,dict):
@@ -14,3 +15,10 @@ def validate_user_preferences():
   if points_value_label not in [True, False]:
      raise ValueError("<points_value_label> must be a bool")
   
+def validate_os():
+   if not platform == "linux" and not platform == "linux2":
+      raise OSError("This software is supported on Linux. WSL installation needed for Windows users.") 
+
+def validate_parameters():
+   validate_os()
+   validate_user_preferences()
